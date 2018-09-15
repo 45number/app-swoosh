28b90919619d6117a41b33418e665f7f363ce2e6
Admin <admin@Mac-Admin.local>
2018-09-15 20:53:32 +0300

    some minor changes

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Admin on 14.09.2018.
+//  Copyright © 2018 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
